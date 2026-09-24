8b710ba3c610c86d0b9d156f5bd332bd1d9d1b3f
Morenza Saumanuk <morenzasaumanuk@gmail.com>
2026-09-24 15:47:48 +0700

    Tambah fitur daftar produk

---
+++ src/main.py
@@ -0,0 +1,32 @@
+produk = [
+    {
+        "id": 1,
+        "nama": "Laptop",
+        "harga": 7500000
+    },
+    {
+        "id": 2,
+        "nama": "Mouse",
+        "harga": 150000
+    },
+    {
+        "id": 3,
+        "nama": "Keyboard",
+        "harga": 300000
+    }
+]
+
+
+def daftar_produk():
+    print("=== DAFTAR PRODUK ===")
+
+    for item in produk:
+        print(
+            f"{item['id']}. "
+            f"{item['nama']} - "
+            f"Rp{item['harga']:,}"
+        )
+
+
+if __name__ == "__main__":
+    daftar_produk()
